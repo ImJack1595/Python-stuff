f0c6638ead0a9dca01f1f45fd6186893bcab9986
ImJack1595 <minhjduc153@gmail.com>
2021-07-31 10:33:50 +0700

    5 files

---
+++ Coding/Python basic/draw-circle-game.py
@@ -0,0 +1,16 @@
+from turtle import Screen
+from typing import Sized
+import pygame as py 
+py.init()
+size = (500, 300)
+Screen = py.display.set_mode(size)
+
+while True:
+    for ev in py.event.get():
+        if ev.type == py.MOUSEBUTTONUP:
+            pos = py.mouse.get_pos()
+            col = (0, 255, 255)
+            py.draw.circle(
+                Screen, col, pos, 20, 5
+            )
+            py.display.update()
